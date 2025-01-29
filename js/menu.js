ebeac9e6d11779d3e9c6d300b20ce0fe6b7bb2d0
Terrence Breschi <tbreschi3@gmail.com>
2025-01-29 00:41:22 -0500

    updated about page and grid layouts

--- js/menu.js
+++ js/menu.js
@@ -5,5 +5,6 @@ function initializeMenu() {
   navToggle?.addEventListener('click', () => {
     mobileMenu.classList.toggle('is-open');
     navToggle.classList.toggle('is-active');
+    // Ensure footer visibility is controlled by CSS
   });
 } 
